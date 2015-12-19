e1d1fcf8594cd666b8dc7b1cd9723ac0084b707c
walakka-jp <walakka.jp@gmail.com>
2015-12-19 12:12:28 +0000

    edited fix assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-# for Bootstrap fonts 
-# refer to: http://gascar-shunt.tumblr.com/post/65236279971/rails-4%E3%81%A7bootstrap-3-glyphicon%E5%95%8F%E9%A1%8C%E3%82%82%E8%A7%A3%E6%B1%BA
-config.assets.precompile +=  %w( *.woff *.eot *.svg *.ttf )
+Rails.application.config.assets.precompile += %w( *.woff *.eot *.svg *.ttf )
